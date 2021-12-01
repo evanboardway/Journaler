7f068dd7521dbb710332b2054fa05c5fdf0821f2
Evan Boardway <evan@Evans-Mac-Pro.local>
2021-12-01 18:53:41 -0500

    added weather stuff

--- public/scripts/geolocation.js
+++ public/scripts/geolocation.js
@@ -4,19 +4,25 @@ const newEntrySubmit = document.getElementById('newEntrySubmit')
 
 var request = new XMLHttpRequest();
 
-request.onload = (response) => {
-  console.log("resp", response)
-  weatherInput.value="Unavailible"
+request.onload = () => {
+  if (request.status != 200) {
+    weatherInput.value="Unavailible"
+  } else {
+    parsed = JSON.parse(request.response)
+    weatherInput.value = parsed.weather[0].description
+  }
   newEntrySubmit.disabled = false
+  newEntrySubmit.innerText = "SUBMIT"
 }
 
 const apiKey = "ddf41b6a9c50f295b6389305e6ddd2a5"
 
 newEntrySubmit.disabled = true
+newEntrySubmit.innerText = "Just a sec... gathering weather information"
 
 navigator.geolocation.getCurrentPosition((position) => {
   locationInput.value = `${position.coords.latitude},${position.coords.longitude}`
 
-  request.open('GET', `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}`)
+  request.open('GET', `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&exclude=daily,hourly,minutely,alerts&appid=${apiKey}`)
   request.send()
 });
